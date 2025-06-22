6a32e2cecf2d9ad3b2ae2ac49cecb743251abd5d
Łukasz Andryszewski <lukasz.andryszewski@outlook.com>
2025-06-23 01:18:28 +0200

    elitism fixes

--- src/utils/elitism.py
+++ src/utils/elitism.py
@@ -1,13 +1,14 @@
 
 class SaveBest:
-    def __init__(self):
+    def __init__(self, toolbox):
         self.best_ind = None
+        self.toolbox = toolbox
 
-    def check(self, ind, values, toolbox):
-        ind_clone = toolbox.clone(ind)
+    def check(self, ind, values):
+        ind_clone = self.toolbox.clone(ind)
         ind_clone.fitness.values = values
         if self.best_ind == None or self.best_ind.fitness < ind_clone.fitness:
             self.best_ind = ind_clone
 
-    def get_best(self, *args):
+    def get_best(self, *args, **kwargs):
         return self.best_ind
